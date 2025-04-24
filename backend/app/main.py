d5e202e30539c38abefcaf56651bc419ab3dd717
Austin Papritz <austin@papritz.dev>
2025-04-24 15:25:14 -0700

    added init_db to main.py commented out router stuff for now

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.db.init_db import init_db
 from app.api.v1.routes import users
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+      init_db()
+      
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -12,4 +17,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(users.router, prefix="/api/v1")
+# app.include_router(users.router, prefix="/api/v1")
