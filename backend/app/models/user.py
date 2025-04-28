a83695d719acf73ef7c2fd4e99f892dd4d58e70e
Austin Papritz <austin@papritz.dev>
2025-04-28 13:34:32 -0700

    updated User model and created schemas folder

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from datetime import datetime
 from app.db.base import Base
 
 class User(Base):
@@ -7,3 +8,13 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
+    
+    bio = Column(String, nullable=True)
+    profile_image_url= Column(String, nullable=True)
+    cover_image_url= Column(String, nullable=True)
+    last_login = Column(DateTime, nullable=True)
+    is_active = Column(Boolean, default=True)
+    role = Column(String, default="user")
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    
